7f6f589177bd0062bb525b2b20d36ad5885dbf0a
Alexey <exelay.rapik@gmail.com>
2020-11-28 22:37:54 +0300

    Wrote simple is_alive_host function

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-"""
-app example
-
-"""
+import requests
 
 
 def is_alive_host(hostname):
     """Проверить, что запрашиваемый хост возвращает http status 100<=x<400."""
-    pass
+    url = 'http://' + hostname
+    try:
+        response = requests.get(url)
+        status_code = response.status_code
+        if 100 <= status_code <= 400:
+            return True
+    except Exception:
+        return False
