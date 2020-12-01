df4807a4915bbf2dba731faf6225e6b4fe97444e
Alexey <exelay.rapik@gmail.com>
2020-12-01 17:01:57 +0300

    Added docstrings in the tests

--- tests.py
+++ tests.py
@@ -2,16 +2,20 @@ from app import is_alive_host
 
 
 def test_live():
+    """Check response if host is alive."""
     assert is_alive_host('semrush.com')
 
 
 def test_down():
+    """Checks response if the host is down."""
     assert not is_alive_host('invalid.domain.son')
 
 
 def test_invalid_url():
+    """Checks response if the hostname is empty."""
     assert not is_alive_host('')
 
 
 def test_404():
+    """Checks response if the host return 404 status."""
     assert not is_alive_host('app.drom.ru')
